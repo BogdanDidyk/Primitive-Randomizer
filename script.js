b555b37f7be6976d1c4ca8b1f68f380f3c231b39
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-10-07 23:49:32 +0300

    Added function that returns random float

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function getRandomInteger(min = 0, max = 9) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function getrandomFloat(min = 0, max = 9) {
+    return Math.random() * (max - min) + min;
+}
