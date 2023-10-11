6b553f471911d3f392b3d183a0529bcfb8f3be00
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-10-11 20:34:35 +0300

    Added a usage example

--- script.js
+++ script.js
@@ -55,3 +55,53 @@ function getRandomString(length, randomStringOptions = {}) {
     
     return str;
 }
+
+
+function getRandomIntegerArray(length, min, max) {
+    return Array.from({length}, () => getRandomInteger(min, max));
+}
+
+function getRandomUpperLatinLettersArray(length) {
+    return Array.from({length}, () => getRandomUpperLatinLetter());
+}
+
+function getRandomStringArray(arrLength, strLength, randomStringOptions) {
+    return Array.from({length: arrLength}, () => getRandomString(strLength, randomStringOptions));
+}
+
+function getRandomIntegerMatrix(rows, cols, min, max) {
+    return Array.from({length: rows}, () => getRandomIntegerArray(cols, min, max));
+}
+
+function getRandomUpperLatinMatrix(rows, cols) {
+    return Array.from({length: rows}, () => getRandomUpperLatinLettersArray(cols));
+}
+
+function printArray(arr, separator = ", ") {
+    console.log(arr.join(separator));
+}
+
+function printMatrix(matrix, itemsSparator) {
+    matrix.forEach(row => printArray(row, itemsSparator));
+}
+
+const randomIntArr = getRandomIntegerArray(10, -1, 1);
+printArray(randomIntArr);
+console.log("");
+
+const randomIntMatrix = getRandomIntegerMatrix(3, 4, 10, 99);
+printMatrix(randomIntMatrix, " ");
+console.log("");
+
+const randomUpperLatinMatrix = getRandomUpperLatinMatrix(5, 5);
+printMatrix(randomUpperLatinMatrix, "_");
+console.log("");
+
+const randomStringOptions = {
+    useDigits: true,
+    useLowerLatinLetters: true,
+    useUpperLatinLetters: false,
+    useSpecialCharacters: true
+};
+const randomStringArr = getRandomStringArray(7, 10, randomStringOptions);
+printArray(randomStringArr, "\n");
